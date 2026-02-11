6a6dc7e310a86d4fc2ce9a2fba4a8ef98240d544
gdcraft07 <iklass987654321@gmail.com>
2026-02-11 22:52:27 +0300

    learn closure funk

---
+++ closure.py
@@ -0,0 +1,14 @@
+def make_multiplier(n):
+    def multiplier(x):
+        return x * n
+    
+    return multiplier
+
+
+double = make_multiplier(2)
+triple = make_multiplier(3)
+
+print(double(5))
+print(triple(5))
+
+print(make_multiplier(5)(13))
