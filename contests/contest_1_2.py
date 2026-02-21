1a343d3e2af7c56adf5a4b70edb4fa5a12ab64dd
gdcraft07 <iklass987654321@gmail.com>
2026-02-21 20:10:33 +0300

    small change

--- contests/contest_1_2.py
+++ contests/contest_1_2.py
@@ -1,30 +1,30 @@
 # 1
 
-# print("************" + input()[-4:])
+print("************" + input()[-4:])
 
 # 2
 
-# price = int(input()); print(price * (1 - 0.10 * (price > 1000)))
+price = int(input()); print(price * (1 - 0.10 * (price > 1000)))
 
 # 3
 
-# n = int(input()); print(f"{str(n // 60).zfill(2)}:{str(n % 60).zfill(2)}")
+n = int(input()); print(f"{str(n // 60).zfill(2)}:{str(n % 60).zfill(2)}")
 
 # 4
 
-# print(((int(input()) - 1) // 100) + 1)
+print(((int(input()) - 1) // 100) + 1)
 
 # 5
 
-# number = int(input()); print(number + 2 ** ((number + 1) % 2))
+number = int(input()); print(number + 2 ** ((number + 1) % 2))
 
 # 6
 
-# print('-'.join(input().lower().split()))
+print('-'.join(input().lower().split()))
 
 # 7
 
-# print(f"{float(input()) * ((1 + float(input()) / 100) ** float(input())):.2f}")
+print(f"{float(input()) * ((1 + float(input()) / 100) ** float(input())):.2f}")
 
 # 8
 
